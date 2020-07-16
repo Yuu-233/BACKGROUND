19b13eb4537b8bb1958a4538e58fe38b653833f7
Moldir Polat <moldir.polat@yandex.com>
2020-07-16 13:53:10 +0600

    added CrossOrigin

--- backend/src/main/java/com/example/demo/controller/ResumeController.java
+++ backend/src/main/java/com/example/demo/controller/ResumeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class ResumeController {
 
